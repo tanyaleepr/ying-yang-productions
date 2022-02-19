798df0dd916e57b065b70ef9e135cfe271cd8248
tanyaleepr <tanyaleepr@gmail.com>
2022-02-18 23:04:41 -0600

    added the missing index file and the function for generate the html

--- index_script.js
+++ index_script.js
@@ -194,5 +194,18 @@ const addEmployee = () => {
 
 };
 
+// generateHTML.js : 
+const writeFile = data => {
+    fs.writeFile('../dist/index.html', data, err => {
+
+        if (err) {
+            console.log(err);
+            return;
+
+        } else {
+            console.log("Your team profile has been created. Please check out your index.html")
+        }
+    })
+}; 
 
 
